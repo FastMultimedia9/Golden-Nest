b2c7861196f2f7cd0f1d0c2bac5de27ba19cee14
FastMultimedia9 <fasttech227@gmail.com>
2025-12-11 03:02:53 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
+import { SpeedInsights } from "@vercel/speed-insights/react"; // Import added here
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
@@ -266,6 +267,8 @@ function App() {
               </>
             } />
           </Routes>
+          {/* SpeedInsights component added here */}
+          <SpeedInsights />
         </main>
         <Footer />
       </div>
